cefe367aa75de2b83eaedc8f9d6c3641b9e249dc
bhupey <bhupendra.neupane08@gmail.com>
2024-01-07 18:02:12 +0545

    development

---
+++ MauiApp1/Data/WorkerPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data
+{
+    public class WorkerPage
+    {
+        public string Role { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ MauiApp1/Services/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Linq;
+using System.Text;
+using MauiApp1.Data;
+namespace MauiApp1.NewFolder
+{
+    public class User
+    {
+        private List<WorkerPage> userList = new()
+        {
+            new WorkerPage()
+            {
+                Password = "admin",
+                Role = "admin",
+            },
+
+            new WorkerPage()
+            {
+                Password = "staff",
+                Role = "staff"
+            }
+        };
+
+        public WorkerPage LoginWorker(string password)
+        {
+            WorkerPage worker = userList.FirstOrDefault(w => w.Password == password);
+
+            if (worker == null)
+            {
+                throw new Exception("Invalid Password");
+            }
+
+            return worker;
+        }
+    }
+}
